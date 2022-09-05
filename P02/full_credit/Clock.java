236ab9ee15df847933e702e3ef10e3c35ea62ad1
Miguel <mt353469@gmail.com>
2022-09-05 18:34:05 -0500

    Clock

--- P02/full_credit/Clock.java
+++ P02/full_credit/Clock.java
@@ -3,8 +3,21 @@ import java.time.Clock;
 public class Clock{ 
     public void clock{ 
     
+        int seconds; 
+        int minutes; 
+        int hours; 
+        
+        //Clock()
     
     
     
                      } 
+                     
+     public static void main(String []args){ 
+         System.out.println("Testing");          
+          
+          
+                                            }    
+                     
+                     
                   }
