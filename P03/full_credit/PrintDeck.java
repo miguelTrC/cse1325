019cd27eea541a606f802b9a2cde060e2bc16a45
Miguel <mt353469@gmail.com>
2022-09-08 11:56:31 -0500

    Start here

---
+++ P03/full_credit/PrintDeck.java
@@ -0,0 +1,15 @@
+/* 
+Main() goes in here 
+
+shiffle the 'Deck' and print out each 'Card' 
+
+Can't access Deck.deck bc its encapsulated, use Deck's method instead. 
+
+Beggining: 
+See what encapsulated means
+
+
+
+
+NEED A WORKING XML FILE 
+*/
