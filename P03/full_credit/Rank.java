8065930c78dcd1d6cb357e39a8328fe293d0ab01
Miguel <mt353469@gmail.com>
2022-09-08 11:24:11 -0500

    Rank

---
+++ P03/full_credit/Rank.java
@@ -0,0 +1,8 @@
+/*
+Write a class that encapsulate an int between 0 and 9 
+Add a public staticfinal ints MIN (0) and MAX(9) to declare the range. 
+In the constructor, initialize the 'rank' field to the parameters 
+Rank(rank : int)
+toString()          <-- returns the string representation of 'rank' field
+
+*/
