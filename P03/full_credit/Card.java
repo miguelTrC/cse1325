d241bdf1afa5e194c19706e59164b67c5d8ca91c
Miguel <mt353469@gmail.com>
2022-09-08 11:33:40 -0500

    Card

---
+++ P03/full_credit/Card.java
@@ -0,0 +1,12 @@
+/* 
+Write a class that encapsulates a Suit and a Rank 
+The constructor assigns its parameters to the corresponding fields. 
+
+Card(rank : Rank, suit, Suit) 
+toString() : string       <--- returns rank field first then suit (no space 
+example: 3U or 0T)
+
+
+
+
+*/ 
