a16215c2cb8913c3579eed9c1892f542ed94b535
Miguel <mt353469@gmail.com>
2022-09-13 05:27:38 -0500

    work

--- P03/full_credit/Deck.java
+++ P03/full_credit/Deck.java
@@ -1,17 +1,20 @@
 import java.util.Stack;
 import java.util.Collections;
 public class Deck{
-	Stack<String> cardLetter = new Stack<String>();
+	public void deck(){
+	
+	
+		Stack<String> cardLetter = new Stack<String>();
+			
+		int count = 0; 
+		while(count < MAX){
+			cardLetter.add(count + "A");
+			cardLetter.add(count + "T");
+			cardLetter.add(count + "U");
+			count++;
+		}
 		
-	int count = 0; 
-	while(count < MAX){
-		cardLetter.add(count + "A");
-		cardLetter.add(count + "T");
-		cardLetter.add(count + "U");
-		count++;
-	}
-	
-	
+			}
 	
 }
 
