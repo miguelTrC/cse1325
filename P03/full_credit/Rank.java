100be2bde09e4ec0aaf338dbd69b15eda868774d
Miguel <mt353469@gmail.com>
2022-09-13 03:10:24 -0500

    final

--- P03/full_credit/Rank.java
+++ P03/full_credit/Rank.java
@@ -1,15 +1,18 @@
+import java.util.Random; 
 public class Rank{ 
 	
 	int rank; 
-	final int MIN = 0;
-	final int MAX = 9; 	
+	static final int MIN = 0;
+	static final int MAX = 9; 	
 	
-	Rank(int rank){
-	this.rank = rank; 
+	Random rand;
+	
+	Rank(){
+	rank = rand.nextInt((MAX - MIN) + 1) + MIN; 
 	}
 	
 	public String toString(){ 
-		return rank;
+		return rank + "";
 	}
 	
 	
