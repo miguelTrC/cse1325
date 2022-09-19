965a340b17ff3e2a659b7a1743b270601b9e102d
Miguel <mt353469@gmail.com>
2022-09-19 11:53:02 -0500

    wip

---
+++ P04/full_credit/GasVehicle.java
@@ -0,0 +1,18 @@
+public class GasVehicle extends Vehicle{ 
+	static double dollarsPerGallonOfGas; 
+	double milesPerGallon; 
+	double gallonsInTank; 
+	// BodyStyle??? 
+	
+	GasVehicle(int year, String brand, String model, double milesPerGallon, 
+	double gallonsInTank){
+	// BodyStyle???
+	this.year = year; 
+	this.brand = brand; 
+	this.model = model; 
+	this.milesPerGallon = milesPerGallon; 
+	this.gallonsInTank = gallonsInTank; 
+	
+	}	
+	
+	}
