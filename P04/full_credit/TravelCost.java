0f8350fdbe0ac90db3a866dad93b2aaae234b958
Miguel <mt353469@gmail.com>
2022-09-19 12:40:50 -0500

    wip

---
+++ P04/full_credit/TravelCost.java
@@ -0,0 +1,10 @@
+/* 
+	Will insert the table of car info here, into an arrayList, 
+	
+	Will read in info into the classes/ variables
+	
+	
+	NOTE* looks at how the enums are declared in the info sheet? 
+	
+
+*/
