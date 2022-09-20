5c3cb1c897f49fded3903a25f9aa2b20ad285273
Miguel <mt353469@gmail.com>
2022-09-20 06:41:28 -0500

    testing phase

--- P04/full_credit/ElectricVehicle.java
+++ P04/full_credit/ElectricVehicle.java
@@ -5,18 +5,15 @@ public class ElectricVehicle extends Vehicle{
 	
 	ElectricVehicle(int year, String brand, String model, BodyStyle bodyStyle, 
 	double whPerMile, double kwhInBattery){
+	
 	super(year, brand, model, bodyStyle);
-	//this.year = year; 
-	//this.brand = brand; 
-	//this.model = model; 
-	//this.bodyStyle = bodyStyle; 
+	
 	this.whPerMile = whPerMile; 
 	this.kwhInBattery = kwhInBattery;
 	
 	}
 	
 	
-	// not 100% if this is the correct method, or if they should be double
 	
 	public double range(){
 		double range = kwhInBattery / (whPerMile / 1000); 
@@ -32,8 +29,7 @@ public class ElectricVehicle extends Vehicle{
 		return (fuelConsumed); 
 		
 	}
-	//Unsure if I should re-use miles as a return, maybe doesn't matter? 
-	//printf(dolllarsToTravel(valueHere))
+
 	public double dollarsToTravel(double miles){
 		double dollarsToTravel = fuelConsumed(miles) * (centsPerKwhOfElectricity / 100);
 		return (dollarsToTravel); 
@@ -42,22 +38,4 @@ public class ElectricVehicle extends Vehicle{
 }
 
 
-/*
 
-range() 
-	kwhInBattery / (whPerMile / 1000)
-	returns a double 
-	
-fuelConsumed(double miles)
-	miles * (whPerMile / 1000)
-	if fuelConsumed > kwhInBattery, throw an error msg/exception
-	
-dollarsToTravel(double miles) 
-	fuelConsumed(miles) * (centsPerKwhOfElectricity / 100)
-
-
-I think that I need to make these Methods (functions), and then run them in 
-the Vehicle file?
-
-
-*/
