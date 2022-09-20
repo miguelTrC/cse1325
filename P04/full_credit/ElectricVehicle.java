7491717305030f99074f6d62908e4e177ea7096d
Miguel <mt353469@gmail.com>
2022-09-20 05:00:49 -0500

    test

--- P04/full_credit/ElectricVehicle.java
+++ P04/full_credit/ElectricVehicle.java
@@ -1,17 +1,17 @@
 public class ElectricVehicle extends Vehicle{ 
 	static double centsPerKwhOfElectricity; 
 	double whPerMile; 
-	double kwhInBaterry; 
+	double kwhInBattery; 
 	
 	ElectricVehicle(int year, String brand, String model, BodyStyle bodyStyle, 
-	double whPerMile, double kwhInBaterry){
+	double whPerMile, double kwhInBattery){
 	
 	this.year = year; 
 	this.brand = brand; 
 	this.model = model; 
 	this.bodyStyle = bodyStyle; 
 	this.whPerMile = whPerMile; 
-	this.kwhInBatterry = kwhInBatterry;
+	this.kwhInBattery = kwhInBattery;
 	
 	}
 	
@@ -19,21 +19,24 @@ public class ElectricVehicle extends Vehicle{
 	// not 100% if this is the correct method, or if they should be double
 	
 	public double range(){
-		double range = kwhInBaterry / (whPerMile / 1000); 
+		double range = kwhInBattery / (whPerMile / 1000); 
 		return range; 
 	}
 		
 	public double fuelConsumed(double miles){
-		miles = miles * (whPerMile / 1000);
-		//need to add error msg
-		return miles; 
+		double fuelConsumed = miles * (whPerMile / 1000);
+		if (fuelConsumed > kwhInBattery){
+			System.out.println("Error: More battery consumed than available ");
+			fuelConsumed = 0.0; 
+		}
+		return fuelConsumed; 
 		
 	}
 	//Unsure if I should re-use miles as a return, maybe doesn't matter? 
 	//printf(dolllarsToTravel(valueHere))
 	public double dollarsToTravel(double miles){
-		fuelConsumed(miles) * (centsPerKwhOfElectricity / 100);
-		return miles; 
+		double dollarsToTravel = fuelConsumed(miles) * (centsPerKwhOfElectricity / 100);
+		return dollarsToTravel; 
 	}
 
 }
