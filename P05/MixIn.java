70ea0094b175c18a29b54343925f8379ebb508d5
Miguel <mt353469@gmail.com>
2022-10-03 15:18:13 -0500

    Continue here

--- P05/MixIn.java
+++ P05/MixIn.java
@@ -1,5 +1,17 @@
-public class MixIn extends Item{
-	MixIn(String name, String description, int cost, int price){
-		super(name, description, cost, price); 
-	}
+/*
+	MixInFlaor 
+	MixInAmount = normal, making a a enum = new enum? 
+	
+	both private 
+	
+	public constructor to initilizae em
+	
+	
+	ToString 
+
+*/
+public class MixIn{
+	private MixInFlavor flavor = new MixInFlavor();
+	private MixInAmount amount = MixInAmount.NORMAL;
+	
 }
