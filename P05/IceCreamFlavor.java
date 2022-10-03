f638e4f62d319366fbfeb727b98e36d751a2924d
Miguel <mt353469@gmail.com>
2022-10-03 13:55:41 -0500

    wip

--- P05/IceCreamFlavor.java
+++ P05/IceCreamFlavor.java
@@ -1,1 +1,17 @@
-//
+/*
+6943 restroom 
+
+No fields but a constructor? is this a sub? 
+
+
+Maybe this is a sub class of item, and a constructor just initializes 
+super (x,y)
+
+*/ 
+
+public class IceCreamFlavor extends Item.java { 
+	
+	
+		
+		
+	}
