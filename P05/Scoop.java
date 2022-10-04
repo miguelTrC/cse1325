a291697e53e941734c76ba1316e28f37b529d00c
Miguel <mt353469@gmail.com>
2022-10-04 00:31:49 -0500

    wip

--- P05/Scoop.java
+++ P05/Scoop.java
@@ -1,1 +1,26 @@
-//Need to still add everything
+/*
+	prv IceCreamFlavor flavor by constructor 
+	
+	prv and arraylist of MixIn choices.
+	add to the arrayList creating a addMixIn
+	
+	toString 
+	returns flavor 
+	
+	or if it has mix ins 
+	returns flavor + mixIn0, + mixIn1 ... 
+	
+	public Scoop constructor 
+	
+	pbl addMixIn
+
+*/
+
+public class Scoop{
+	private IceCreamFlavor flavor = new IceCreamFlavor(); 
+	private ArrayList mixins = new ArrayList(); 
+	
+	public Scoop(IceCreamFlavor flavor){
+		this.flavor = flavor; 
+	}
+}
