d76ba2300bb541c421b43d2c4e942801f5c4e8bb
Miguel <mt353469@gmail.com>
2022-10-04 00:06:09 -0500

    fin

--- P05/MixIn.java
+++ P05/MixIn.java
@@ -1,17 +1,13 @@
-/*
-	MixInFlaor 
-	MixInAmount = normal, making a a enum = new enum? 
-	
-	both private 
-	
-	public constructor to initilizae em
-	
-	
-	ToString 
-
-*/
 public class MixIn{
 	private MixInFlavor flavor = new MixInFlavor();
 	private MixInAmount amount = MixInAmount.NORMAL;
 	
+	MixIn(MixInFlavor flavor, MixInAmount amount){
+		this.flavor = flavor; 
+		this.amount = amount; 
+	}
+	
+	public String toString(){
+		return(flavor + " (" + amount + " )");
+	}
 }
