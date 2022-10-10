dfd4d51fd396e39e08ae6e96aad62894a7aa80e7
Miguel <mt353469@gmail.com>
2022-10-10 16:58:47 -0500

    fin

--- P05/Scoop.java
+++ P05/Scoop.java
@@ -1,21 +1,3 @@
-/*
-	prv IceCreamFlavor flavor by constructor 
-	
-	prv and arraylist of MixIn choices.
-	add to the arrayList creating a addMixIn
-	
-	toString 
-	returns flavor 
-	
-	or if it has mix ins 
-	returns flavor + mixIn0, + mixIn1 ... 
-	
-	public Scoop constructor 
-	
-	pbl addMixIn
-
-*/
-
 public class Scoop{
 	private IceCreamFlavor flavor; 
 	private ArrayList<MixIn> mixins; 
@@ -24,11 +6,11 @@ public class Scoop{
 		this.flavor = flavor; 
 		this.mixins = new ArrayList<>();
 	}
-/*	//MixIn mixins?
+	//MixIn mixins?
 	public void addMixIn(MixIn mixin){
 		mixins.add(mixin);
 	}
-	*/
+	
 	
 	// ToString method from Professor Rice
 	public String toString(){
