079669eab44b3b0ef6b591d0e3bcb5436e2017ad
Miguel <mt353469@gmail.com>
2022-10-11 03:04:22 -0500

    wip

---
+++ P06/Emporium.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Emporium {
+// ArrayLIst of mixInFlavor(s) iceCreamFlavor(s) scoop(s)
+// this.mixins = new ArrayList<>(); 
+	private ArrayList<MixInFlavor> mixInFlavors = new ArrayList<>(); 
+	private ArrayList<IceCreamFlavor> iceCreamFlavor = new ArrayList<>(); 
+	private ArrayList<Scoop> scoops = new ArrayList<>(); 
+	
+	public void addMixInFlavor(MixInFlavor mixInFlavors){
+	
+	}
+	
+
+	
+	
+	
+}
+
+/*
+
+Gives list elements of arrayList?
+
+Objects[]
+ArrayListName.toArray()
+
+*/
+
