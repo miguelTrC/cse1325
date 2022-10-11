548333c937de529ed43ff66ea1a0e12f865f1dd0
Miguel <mt353469@gmail.com>
2022-10-10 19:44:24 -0500

    Change Private to protected

--- P05/Item.java
+++ P05/Item.java
@@ -1,9 +1,9 @@
 public class Item{
 	
-	private String name; 
-	private String description; 
-	private int cost; 
-	private int price; 
+	protected String name; 
+	protected String description; 
+	protected int cost; 
+	protected int price; 
 	
 	public Item(String name, String description, int cost, int price){
 		this.name = name; 
