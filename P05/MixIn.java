599099836caef0f7db3280ae7a5331b7dc37e7b6
Miguel <mt353469@gmail.com>
2022-10-10 19:54:58 -0500

    fin

--- P05/MixIn.java
+++ P05/MixIn.java
@@ -6,9 +6,9 @@ public class MixIn{
 		this.flavor = flavor; 
 		this.amount = amount; 
 	}
-	
+	@Override 
 	public String toString(){
-		return(flavor.toString() + (!amount.equals(MixInAmount.Normal
-			? " (" + amount + ")" : "")));
+		return(flavor.toString() + (!amount.equals(MixInAmount.Normal)
+			? " (" + amount + ")" : ""));
 	}
 }
