7eb7f124659315d964ee1385137e2733b92eef23
Miguel <mt353469@gmail.com>
2022-10-27 11:48:50 -0500

    wip

--- P08/full_credit/hello_all.cpp
+++ P08/full_credit/hello_all.cpp
@@ -6,9 +6,12 @@ Take user input and print 'Hello "UserInput" x3 '
 
 int main() { 
 	
-	std::string name; 
+	std::string name;
+	std::cout << "TEST " << std::endl; 
 	
-	for(int count = 0; count > 3; count++) { 
+	
+	
+	for(int count = 0; count < 3; count++) { 
 		std::cout << "What is your name? " << std::endl; 
 		std::cin >> name; 
 		
