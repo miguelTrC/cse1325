63d286cb7bd0c6391b7d8b7d3054306218b24727
Miguel <mt353469@gmail.com>
2022-10-26 23:37:32 -0500

    wip

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,23 @@
+/*
+Take user input and print 'Hello "UserInput" '
+*/
+
+#include <iostream> 
+
+int main() { 
+
+	// prob a better way
+	char[20] name; 
+	
+	
+	std::cout << "What is your name? " << std::endl; 
+	
+	
+
+	//need notes from C, fscan? 
+	
+	std::cout << name << std::endl; 
+	
+	
+	return EXIT_SUCCESS;	
+}
