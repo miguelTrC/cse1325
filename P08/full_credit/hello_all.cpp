442580c2e51417c73eec12e465cac920387fd874
Miguel <mt353469@gmail.com>
2022-10-31 14:20:57 -0500

    fin

--- P08/full_credit/hello_all.cpp
+++ P08/full_credit/hello_all.cpp
@@ -3,16 +3,13 @@
 int main() { 
 	
 	std::string name;
-	
-	// Loops 4 times 
-	for(int count = 0; count < 4; count++) { 
-		std::cout << "What is your name? " << std::endl; 
-		std::cin >> name; 
+	std::cout << "What is your name? " << std::endl; 
+	std::cin >> name; 
 		
-		std::cout << "Hello " 
-				  << name << std::endl
-				  << std::endl; 
-			  }
+	std::cout << "Hello " 
+			  << name << std::endl
+			  << std::endl; 
+			  
 			   
 	
 	
