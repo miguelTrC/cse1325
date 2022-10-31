cb7e4a77f5f07e4bf3938b376bc27d575c666492
Miguel <mt353469@gmail.com>
2022-10-31 17:40:45 -0500

    fin

--- P08/bonus/reverse.cpp
+++ P08/bonus/reverse.cpp
@@ -1,11 +1,7 @@
 #include <iostream> 
-//#include <string.h>
 #include <algorithm> // std::reverse
-#include <vector> 
 
 int main(int argc, char *args[]) {
-	
-	//std::vector<std::string> input;
 	
 	for (int count = 1; count < argc; count++){ 
 	
