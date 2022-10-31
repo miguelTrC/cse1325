066087d3ef89f0eb257540681280eedb726424a0
Miguel <mt353469@gmail.com>
2022-10-31 17:18:55 -0500

    wip

--- P08/bonus/reverse.cpp
+++ P08/bonus/reverse.cpp
@@ -1,10 +1,20 @@
 /*
-take user input, print it backwards 
+./a.out Hello 
+
+Prints [ olleH  ]
+
 */
 
 #include <iostream> 
+//#include <string.h>
+#include <algorithm> // std::reverse
+#include <vector> 
 
-int main() {
+int main(int argc, char *args[]) {
+	
+	std::vector<std::string> input;
+	
+//	std::cout << input << std::endl; 
 	
 	
 	
