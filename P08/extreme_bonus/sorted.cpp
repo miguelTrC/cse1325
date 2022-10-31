c0c11f8d8b74059da900040935c5f85a98e009fd
Miguel <mt353469@gmail.com>
2022-10-31 18:40:47 -0500

    wip

--- P08/extreme_bonus/sorted.cpp
+++ P08/extreme_bonus/sorted.cpp
@@ -1,13 +1,47 @@
 /*
-takes a .txt file, organizes it alphabetical order
+./a.out textFile.txt 
+
+push the values in the text file onto a vector<string>, 
+
+after all values are in the vector, use the sort function from 
+algorithm, (name.begiin(), name.end() )
+
+Leaving comments since I am still learning file operations in c++, 
+will use as a refrence in future project
+
 */
 
 #include <iostream> 
+#include <vector> 
+#include <algorithm>
+#include <fstream> //read txt file (file operator)
 
-int main() {
+int main(int argc, char *args[] ) {
+	
+	std::fstream file; 
+	// opens the file for reading
+	std::file.open(args[1], ios::in);
+	
+	std::vector<std::string> text_Data;
+	std::string line; 
 	
 	
+	//if the file was able to open 
+	if(file.is_open()){
+	}
+	// Gets a line from the text doc and copies it onto the vector
+		while(getline(file, line)){ 
+			
+			text_Data.push_back(line);
+			//Insert algorithm to order the output
+			std::sort( text_Data.begin(), text_Data.end() );
+			//print 	
+			std::cout << text_Data << std::endl;
+		}
+	}
 	
+	
+	file.close; 
 	
 	return EXIT_SUCCESS;
 }
