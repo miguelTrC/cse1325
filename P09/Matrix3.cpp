96f26e7c9e88590b03b7694e1d21bad564e44200
Miguel <mt353469@gmail.com>
2022-11-01 12:12:31 -0500

    wip

---
+++ P09/Matrix3.cpp
@@ -0,0 +1,17 @@
+/*
+Creating a 3x3 matrix and with input, output, and addition operators
+*/
+
+// can replace 2d int to a double vector 
+private Data int[]][](){
+	
+	//
+
+}
+
+/*
+need to add functions/methods within 
+
+
+
+*/
