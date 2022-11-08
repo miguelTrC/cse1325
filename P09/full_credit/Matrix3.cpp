bfdb39543cc69dfe3010d2427b686fd2002cd17b
Miguel <mt353469@gmail.com>
2022-11-08 04:38:12 -0600

    wip

--- P09/full_credit/Matrix3.cpp
+++ P09/full_credit/Matrix3.cpp
@@ -8,6 +8,10 @@ Make a 2d int array to store the parameters in, and then push the array
 into the vector
 
 
+(m00), data(m01), data(m02)},
+{(m10), data(m11), data(m12)}, 
+{data(m20), data(m21), data(m22)
+
 
 */
 
@@ -22,7 +26,10 @@ class Matrix3{
 		Matrix3(int m00, int m01, int m02,
 				int m10, int m11, int m12,
 				int m20, int m21, int m22): 
-				/* stuck here */ 
+				data{ {(m00), (m01), (m02)},
+					  {(m10), (m11), (m12)}, 
+					  {(m20), (m21), (m22)} 
+					  }
 				{
 					std::vector <int> temp;
 					
