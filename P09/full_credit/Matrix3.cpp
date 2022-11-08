4e8f561133f278c31fe7818eeea8868dd5cd9f47
Miguel <mt353469@gmail.com>
2022-11-07 18:17:03 -0600

    wip

---
+++ P09/full_credit/Matrix3.cpp
@@ -0,0 +1,35 @@
+/*
+Creating a 3x3 matrix and with input, output, and addition operators
+named Data
+*/
+
+#include <iostream> 
+#include <vector>
+
+class Matrix3{ 
+	
+	public: 
+		Matrix3(int)
+		
+		
+		
+	private: 
+		std::vector <std::vector<int>> data;  
+		
+	
+} 
+
+// Constructor that accepts 9 values and adds it to the vector
+
+
+/*
+functions: 
+
+get(int x, int y){
+	returns the int at specified index coordinates. 
+	if x or y are less than 0 or greater than 2, 
+	
+	throw std::runtime_error exeption
+}
+
+*/
