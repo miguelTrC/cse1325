7f51424d38c406986b1761abf87ce777f79061e9
Miguel <mt353469@gmail.com>
2022-11-14 20:03:10 -0600

    done

--- P10/Container.java
+++ P10/Container.java
@@ -9,17 +9,23 @@ public class Container{
 	protected int maxScoops; 
 	
 	public Container(String name, String description, int maxScoops){
-		
 		this.name = name; 
 		this.description = description; 
 		this.maxScoops = maxScoops; 
 		}
 	
 	public Container(BufferedReader br) throws IOException{
-		
 		this.name = br.readLine();
 		this.description = br.readLine(); 
 		this.maxScoops = Integer.parseInt(br.readLine());
+	}
+	
+	public String name() {return name};
+	public String description() {return description};
+	public int maxScoops() {return maxScoops};
+	
+	public String toString(){
+		return (name + ""); 
 	}
 	
 }
