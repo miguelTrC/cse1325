7de34321d302c086598cf830da17725fc70064fd
Miguel <mt353469@gmail.com>
2022-11-22 14:28:46 -0600

    price method added

--- P11/product/MixIn.java
+++ P11/product/MixIn.java
@@ -8,6 +8,29 @@ public class MixIn{
 		this.flavor = flavor; 
 		this.amount = amount; 
 	}
+	
+	public Double price(int amountOfFlavors){
+		switch(amount) {
+			case Light:
+				return (amountOfFlavors * 0.8);
+				break;
+			case Normal: 
+				return (amountOfFlavors * 1.0);
+				break; 
+			case Extra: 
+				return (amountOfFlavors * 1.2);
+				break; 
+			case Drenched: 
+				return (amountOfFlavors * 2.0);
+				break; 
+			default: 
+				System.out.println("No MixInAmount");
+		
+		}
+		
+	}
+	
+	
 	@Override 
 	public String toString(){
 		return(flavor.toString() + (!amount.equals(MixInAmount.Normal)
